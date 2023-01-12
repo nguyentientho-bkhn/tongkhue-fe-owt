353373904524b2f024407233650403f185583c21
Tho Nguyen Tien (CongNghe-HO) <tho.nguyentien@vndirect.com.vn>
2023-01-12 18:04:23 +0700

    refact

--- nguyentientho/js/array/arr.js
+++ nguyentientho/js/array/arr.js
@@ -1,12 +1,32 @@
+console.log(oddNumbersFilter([4, 2, 5, 6, 2, 7]));
 
-function ex1(arr) {
-      const result = arr.map((value, index, array) => {
-        //   console.log(value, 'item');
-        //   console.log(index, 'number');
-        //   console.log(index, 'array')
-          return value % 2;
-      })
-      return result;
+/** ex1 */
+function temp(arr) {
+  const result = arr.map((value, index, array) => {
+    //   console.log(value, 'item');
+    //   console.log(index, 'number');
+    //   console.log(index, 'array')
+    return value % 2;
+  })
+  return result;
+}
+
+/** ex2: oddNumbersNotFilter */ 
+function oddNumbersNotFilter(arr) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 1) {
+      result.push(arr[i]);
+    }
   }
-  
-  console.log(ex1([4,2,5,6,2,7]));
+  return result;
+}
+
+/** ex3 oddNumbersFilter */
+function oddNumbersFilter(arr) {
+  const result = arr.filter(item => {
+    return item % 2 === 1;
+  });
+  return result;
+}
